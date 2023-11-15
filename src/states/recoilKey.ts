f260c18913db222e9018dc802520a00dd81c5bb7
kamiyakoh <nanayou791@gmail.com>
2023-11-16 04:18:20 +0900

    memosState.ts削除

--- src/states/recoilKey.ts
+++ src/states/recoilKey.ts
@@ -2,7 +2,5 @@ export const recoilKey = {
   isAuthState: 'IS_AUTH_STATE',
   categoriesState: 'CATEGORIES_STATE',
   pickCategoriesState: 'PICK_CATEGORIES_STATE',
-  mocksDataState: 'MOCKS_DATA_STATE',
-  memosState: 'MEMOS_STATE',
   menuOptionState: 'MENUOPTION_STATE',
 };
